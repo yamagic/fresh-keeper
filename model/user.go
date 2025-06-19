a1d389607446f5a42510d023addd83e937adb78e
yamagic <yamagic202008@gmail.com>
2025-06-19 22:10:50 +0900

    feat: user model 追加

---
+++ model/user.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint   `json:"id" gorm:"primaryKey"`
+	Email     string `json:"email" gorm:"uniqueIndex;not null"`
+	Password  string `json:"-" gorm:"not null"`
+	Name      string `json:"name" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
+type UserReponse struct {
+	ID        uint   `json:"id"`
+	Email     string `json:"email"`
+	Name      string `json:"name"`
+}
