24016ecfc50d07617fa989a492547a577600bedb
yamagic <yamagic202008@gmail.com>
2025-06-24 09:22:18 +0900

    feat: RouterにProductControllerを統合

--- router/router.go
+++ router/router.go
@@ -1,16 +1,27 @@
-package router 
+package router
 
 import (
 	"expiry_tracker/controller"
+	"os"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, pc controller.IProductController) *echo.Echo {
 	e := echo.New()
-
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 	e.POST("/logout", uc.LogOut)
-
+	p := e.Group("/products")
+	p.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET_KEY")),
+		TokenLookup: "cookie:token",
+	}))
+	p.GET("", pc.GetAllProducts)
+	p.GET("/:productId", pc.GetProductById)
+	p.POST("", pc.CreateProduct)
+	p.PUT("/:productId", pc.UpdateProduct)
+	p.DELETE("/:productId", pc.DeleteProduct)
 	return e
 }
