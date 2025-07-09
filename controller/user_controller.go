b464806c71be112e4723a186fe455afa354d5770
yamagic <yamagic202008@gmail.com>
2025-07-09 09:19:26 +0900

    fix: バックエンドCORS設定を更新   - .envファイルのFE_URL環境変数を参照するよう修正   - フロントエンド開発サーバーとの通信を有効化

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -55,7 +55,7 @@ func (uc *userController) Login(c echo.Context) error {
 	cookie.Expires = time.Now().Add(time.Hour * 24)
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
-	// cookie.Secure = true
+	cookie.Secure = true
 	cookie.HttpOnly = true
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
@@ -69,7 +69,7 @@ func (uc *userController) LogOut(c echo.Context) error {
 	cookie.Expires = time.Now()
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
-	// cookie.HttpOnly = true
+	cookie.Secure = true
 	cookie.HttpOnly = true
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
--- router/router.go
+++ router/router.go
@@ -23,8 +23,8 @@ func NewRouter(uc controller.IUserController, pc controller.IProductController) 
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
-		//CookieSameSite: http.SameSiteNoneMode,
-		CookieSameSite: http.SameSiteDefaultMode,
+		CookieSameSite: http.SameSiteNoneMode,
+		// CookieSameSite: http.SameSiteDefaultMode,
 		//CookieMaxAge:   60,
 	}))
 	e.POST("/signup", uc.SignUp)
